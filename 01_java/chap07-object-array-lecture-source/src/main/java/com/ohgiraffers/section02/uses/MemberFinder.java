7489cc30d685fae4cb0282be53141e91dc8fd2ac
qwes5674 <qwes5674@gmail.com>
2024-05-27 16:53:24 +0900

    객체 배열

---
+++ 01_java/chap07-object-array-lecture-source/src/main/java/com/ohgiraffers/section02/uses/MemberFinder.java
@@ -0,0 +1,12 @@
+package com.ohgiraffers.section02.uses;
+
+public class MemberFinder {
+
+    public Member[] findAllMembers() {
+
+        Member[] foundMembersFromRepository = MemberRepository.findAllMembers();
+        // Member Repository의 필드로 있는 10크기의 Member[]
+
+        return foundMembersFromRepository;
+    }
+}
